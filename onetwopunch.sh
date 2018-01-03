fac57c067c5570b44069557f1bb09c56687173ea
Error418 <anonzak@users.noreply.github.com>
2018-01-03 13:55:55 -0600

    Added overhauled onetwopunch
    Fixed issue with nmap options when only providing one flag
    Fixed issue with banner not displaying single target
    
    added function to remove unicornscan results/directory after scan has completed
    added function to only create backup folder when there are files to backup
    added function to remove "ndir/" and place results in "onetwopunch/"
    
    added ability to specify nmap formats
    added ability to specify multiple nmap options
    added ability to specify directory to save the results to
    added ability to specify one target as an argument

--- onetwopunch.sh
+++ onetwopunch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Colors
 ESC="\e["
@@ -19,14 +19,19 @@ echo ""
 }
 
 function usage {
-    echo "Usage: $0 [-t target IP | -f targets.txt] [-p tcp/udp/all] [-i interface] [-n nmap-options] [-l filepath] [-h]"
+    echo "Usage: $0 [-t target IP | -f targets.txt] [-p tcp/udp/all] [-i interface] [-n nmap-options] [-l filepath] [-o output types] [-h]"
     echo "       -h: Help"
     echo "       -t: IP of target to scan (this or '-f' must be specified)."
     echo "       -f: File containing ip addresses to scan (one per line)."
     echo "       -p: Protocol. Defaults to tcp."
-    echo "       -i: Network interface. Defaults to eth0"
-    echo "       -n: Comma separated NMAP options without hyphens (A,O,sV,etc). Defaults to no options."
-    echo "       -l: Directory to save results to."
+    echo "       -i: Network interface. Defaults to eth0."
+    echo "       -n: Comma separated NMap options without hyphens (A,O,sV,etc). Defaults to no options."
+    echo "       -l: Directory to save results to. Defaults to ~/.onetwopunch."
+    echo "       -o: Filetype for reports. Multiple (comma separated) options can be specified. Defaults to Normal."
+    echo "            n: Normal output format."
+    echo "            x: XML output format."
+    echo "            g: Grepable output format."
+    echo "            a: All output formats."
     echo ""
 }
 
@@ -34,23 +39,24 @@ function scans {
     echo -e "${BLUE}[+]${RESET} Scanning $ip for $proto ports..."
 
     # unicornscan identifies all open TCP ports
-    if [[ $proto == "tcp" || $proto == "all" ]]; then 
+    if [[ $proto == "tcp" || $proto == "all" ]]; then
         echo -e "${BLUE}[+]${RESET} Obtaining all open TCP ports using unicornscan..."
         echo -e "${BLUE}[+]${RESET} unicornscan -i ${iface} -mT ${ip}:a -l ${log_dir}/udir/${ip}-tcp.txt"
         unicornscan -i ${iface} -mT ${ip}:a -l ${log_dir}/udir/${ip}-tcp.txt
         ports=$(cat "${log_dir}/udir/${ip}-tcp.txt" | grep open | cut -d"[" -f2 | cut -d"]" -f1 | sed 's/ //g' | tr '\n' ',')
-        if [[ ! -z $ports ]]; then 
+        if [[ ! -z $ports ]]; then
             # nmap follows up
             echo -e "${GREEN}[*]${RESET} TCP ports for nmap to scan: $ports"
-            echo -e "${BLUE}[+]${RESET} nmap -e ${iface} ${nmap_opt} -oX ${log_dir}/ndir/${ip}-tcp.xml -oG ${log_dir}/ndir/${ip}-tcp.grep -p ${ports} ${ip}"
-            nmap -e ${iface} ${nmap_opt} -oN ${log_dir}/ndir/${ip}-tcp.nmap -oG ${log_dir}/ndir/${ip}-tcp.grep -oX ${log_dir}/ndir/${ip}-tcp.xml -p ${ports} ${ip}
+            ntcp+=" -p ${ports}"
+            echo -e "${BLUE}[+]${RESET} $ntcp"
+            $ntcp
         else
             echo -e "${RED}[!]${RESET} No TCP ports found"
         fi
     fi
 
     # unicornscan identifies all open UDP ports
-    if [[ $proto == "udp" || $proto == "all" ]]; then  
+    if [[ $proto == "udp" || $proto == "all" ]]; then
         echo -e "${BLUE}[+]${RESET} Obtaining all open UDP ports using unicornscan..."
         echo -e "${BLUE}[+]${RESET} unicornscan -i ${iface} -mU ${ip}:a -l ${log_dir}/udir/${ip}-udp.txt"
         unicornscan -i ${iface} -mU ${ip}:a -l ${log_dir}/udir/${ip}-udp.txt
@@ -58,12 +64,55 @@ function scans {
         if [[ ! -z $ports ]]; then
             # nmap follows up
             echo -e "${GREEN}[*]${RESET} UDP ports for nmap to scan: $ports"
-            echo -e "${BLUE}[+]${RESET} nmap -e ${iface} ${nmap_opt} -sU -oX ${log_dir}/ndir/${ip}-udp.xml -oG ${log_dir}/ndir/${ip}-udp.grep -p ${ports} ${ip}"
-            nmap -e ${iface} ${nmap_opt} -sU -oN ${log_dir}/ndir/${ip}-udp.nmap -oG ${log_dir}/ndir/${ip}-udp.grep -oX ${log_dir}/ndir/${ip}-udp.xml -p ${ports} ${ip}
+            nudp+=" -sU -p ${ports}"
+            echo -e "${BLUE}[+]${RESET} $nudp"
+            $nudp
         else
             echo -e "${RED}[!]${RESET} No UDP ports found"
         fi
     fi
+}
+
+function clean {
+    echo -e "${BLUE}[+]${RESET} Cleaning up..."
+    rm -rf $log_dir/udir
+    echo -e "${BLUE}[+]${RESET} Done cleaning!"
+}
+
+function build_scan {
+    nscan="nmap -e ${iface} ${nmap_opt} ${ip}"
+
+    #build nmap tcp command with output options
+        #I'm sure there is a much better way to do this, but I am not a programmer...
+    ntcp=$nscan
+    if [[ $output == *"n"* ]]; then
+        ntcp+=" -oN ${log_dir}/${ip}-tcp.nmap"
+    fi
+    if [[ $output == *"x"* ]]; then
+        ntcp+=" -oX ${log_dir}/${ip}-tcp.xml"
+    fi
+    if [[ $output == *"g"* ]]; then
+        ntcp+=" -oG ${log_dir}/${ip}-tcp.grep"
+    fi
+    if [[ $output == *"a"* ]]; then
+        ntcp+=" -oA ${log_dir}/${ip}-tcp"
+    fi
+
+    #build nmap udp command with output options
+    nudp=$nscan
+    if [[ $output == *"n"* ]]; then
+        nudp+=" -oN ${log_dir}/${ip}-udp.nmap"
+    fi
+    if [[ $output == *"x"* ]]; then
+        nudp+=" -oX ${log_dir}/${ip}-udp.xml"
+    fi
+    if [[ $output == *"g"* ]]; then
+        nudp+=" -oG ${log_dir}/${ip}-udp.grep"
+    fi
+    if [[ $output == *"a"* ]]; then
+        nudp+=" -oA ${log_dir}/${ip}-udp"
+    fi
+
 }
 
 banner
@@ -74,7 +123,7 @@ if [[ ! $(id -u) == 0 ]]; then
 fi
 
 if [[ -z $(which nmap) ]]; then
-    echo -e "${RED}[!]${RESET} Unable to find nmap. Install it and make sure it's in your PATH   environment"
+    echo -e "${RED}[!]${RESET} Unable to find nmap. Install it and make sure it's in your PATH environment"
     exit 1
 fi
 
@@ -91,13 +140,13 @@ fi
 # commonly used default options
 proto="tcp"
 iface="eth0"
-nmap_opt="-sV"
+nmap_opt=""
 target=""
 targets=""
 log_dir="${HOME}/.onetwopunch"
 
 
-while getopts "p:i:t:f:n:l:h" OPT; do
+while getopts "p:i:t:f:n:l:o:h" OPT; do
     case $OPT in
         p) proto=${OPTARG};;
         i) iface=${OPTARG};;
@@ -105,17 +154,27 @@ while getopts "p:i:t:f:n:l:h" OPT; do
 	f) targets=${OPTARG};;
         n) nmap_opt=${OPTARG};;
 	l) log_dir=${OPTARG};;
+        o) output=${OPTARG};;
         h) usage; exit 0;;
         *) usage; exit 0;;
     esac
 done
 
+#make sure a target is specified
 if [[ -z $targets && -z $target ]]; then
     echo "[!] No target file provided"
     usage
     exit 1
 fi
 
+#make sure only one target option is used
+if [[ -n $targets && -n $target ]]; then
+    echo "[!] Please only specify one target option"
+    usage
+    exit 1
+fi
+
+#make sure the protocol is correct
 if [[ ${proto} != "tcp" && ${proto} != "udp" && ${proto} != "all" ]]; then
     echo "[!] Unsupported protocol"
     usage
@@ -125,6 +184,8 @@ fi
 # check if multiple nmap options were specified and format them
 if [[ $nmap_opt == *","* ]]; then
     nmap_opt=$(echo -${nmap_opt} | sed 's/,/ -/g')
+elif [[ -n $nmap_opt ]]; then
+    nmap_opt=$(echo -${nmap_opt})
 fi
 
 # add subdirectory to log_dir and remove any trailing "/" characters
@@ -134,35 +195,38 @@ log_dir=$(echo "${log_dir}/onetwopunch")
 echo -e "${BLUE}[+]${RESET} Protocol : ${proto}"
 echo -e "${BLUE}[+]${RESET} Interface: ${iface}"
 echo -e "${BLUE}[+]${RESET} Nmap opts: ${nmap_opt}"
-echo -e "${BLUE}[+]${RESET} Targets  : ${targets}"
-echo -e "${BLUE}[+]${RESET} Log dir  : ${log_dir}"
-
-# backup any old scans before we start a new one
-mkdir -p "${log_dir}/backup/"
-if [[ -d "${log_dir}/ndir/" ]]; then 
-    mv "${log_dir}/ndir/" "${log_dir}/backup/ndir-$(date "+%Y%m%d-%H%M%S")/"
+if [[ -n $targets ]]; then					#use this if a target file is specified
+	echo -e "${BLUE}[+]${RESET} Targets: ${targets}"
 fi
-if [[ -d "${log_dir}/udir/" ]]; then 
-    mv "${log_dir}/udir/" "${log_dir}/backup/udir-$(date "+%Y%m%d-%H%M%S")/"
-fi 
 
-rm -rf "${log_dir}/ndir/"
-mkdir -p "${log_dir}/ndir/"
-rm -rf "${log_dir}/udir/"
+if [[ -n $target ]]; then					#use this if only one target is specified
+	echo -e "${BLUE}[+]${RESET} Target: ${target}"
+fi
+echo -e "${BLUE}[+]${RESET} Log dir: ${log_dir}"
+
+#backup any old scans
+if [[ -a "${log_dir}/*" ]]; then
+    mkdir -p "${log_dir}/backup/"
+    mv "${log_dir}/*.*" "${log_dir}/backup/$(date "+%Y%m%d-%H%M%S")/"
+fi
+
+#create udir
 mkdir -p "${log_dir}/udir/"
 
 # if there are multiple targets (-f option)
 if [[ -n $targets ]]; then
     while read ip; do
+        build_scan
         scans
     done
 elif [[ -n $target ]]; then
     ip=$target
+    build_scan
     scans
 else
     echo "error dealing with the target/targets variables"
 fi
 
 echo -e "${BLUE}[+]${RESET} Scans completed"
+clean
 echo -e "${BLUE}[+]${RESET} Results saved to ${log_dir}"
-
